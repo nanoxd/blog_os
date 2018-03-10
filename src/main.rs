45704f3cb4001d9099d5ae240eb32a5049e1e5c7
Fernando Paredes <nano@fdp.io>
2018-03-09 22:32:08 -0500

    ✨ Setup bin to run without stdlib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(lang_items)]
+#![no_std]
+#![no_main]
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    loop {}
+}
+
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern "C" fn rust_begin_panic(
+    _msg: core::fmt::Arguments,
+    _file: &'static str,
+    _line: u32,
+    _column: u32,
+) -> ! {
+    loop {}
 }
