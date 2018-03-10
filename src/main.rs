dae9906acf9535ab97ae7050e246e7b7c848946c
Fernando Paredes <nano@fdp.io>
2018-03-10 07:06:16 -0500

    ✨ Display Hello World

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,17 @@
 #![no_std]
 #![no_main]
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *const u8 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
 
