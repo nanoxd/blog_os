e80d59f41ac6711e55d0dfb1ba8a36d9ddfc2e71
Fernando Paredes <nano@fdp.io>
2018-03-10 07:34:45 -0500

    ✨ Handle VGA Buffer better

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -28,3 +28,65 @@ impl ColorCode {
     ColorCode((background as u8) << 4 | (foreground as u8))
   }
 }
+
+#[derive(Debug, Clone, Copy)]
+#[repr(C)]
+struct ScreenChar {
+  ascii_character: u8,
+  color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+struct Buffer {
+  chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+  column_position: usize,
+  color_code: ColorCode,
+  buffer: &'static mut Buffer,
+}
+
+impl Writer {
+  pub fn print_something() {
+    let mut writer = Writer {
+      column_position: 0,
+      color_code: ColorCode::new(Color::Yellow, Color::Black),
+      buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_byte(b'H');
+    writer.write_str("ello");
+  }
+
+  pub fn write_str(&mut self, s: &str) {
+    for byte in s.bytes() {
+      self.write_byte(byte)
+    }
+  }
+
+  pub fn write_byte(&mut self, byte: u8) {
+    match byte {
+      b'\n' => self.new_line(),
+      byte => {
+        if self.column_position >= BUFFER_WIDTH {
+          self.new_line();
+        }
+
+        let row = BUFFER_HEIGHT - 1;
+        let col = self.column_position;
+
+        let color_code = self.color_code;
+        self.buffer.chars[row][col] = ScreenChar {
+          ascii_character: byte,
+          color_code: color_code,
+        };
+        self.column_position += 1
+      }
+    }
+  }
+
+  pub fn new_line(&mut self) {}
+}
